8876ea49676a13a95bf2feecd895daa4c0878575
Jasper Travers <wtravers@ucsd.edu>
2020-08-26 19:04:33 -0700

    dragging and resizing

--- src/main.js
+++ src/main.js
@@ -1,56 +1,33 @@
 //import * as util from './utility.js'
-import {makeCard, styleCard, putCodeMirror, putProseMirror, putButton, putCanvas, putConsole, putLayout, execute, emptyprose}
+import {makeCard, styleCard, putCodeMirror, putProseMirror,
+        putButton, putCanvas, putConsole, putLayout,
+        execute, emptyprose, emptycode, makeDraggable, putContainer}
 from './utility.js'
 
 window.onload = () => {
   let main = document.createElement('main');
   document.body.appendChild(main);
-  let card = makeCard({top: 12, left: 12, width: 120, height: 120}, main)
-  let card2 = makeCard({top: 12, left: 12, width: 120, height: 120}, main)
-  putCodeMirror("// cm",card)
-  putProseMirror(emptyprose, card2);
-  makeDraggable(card);
-  makeDraggable(card2);
-}
-
-function makeDraggable(node, step=12) {
-  //const step = 12; //constrain to 12pt grid
-  node.onmousedown = function (evt) {
-    node.dataset.startX = parseInt(evt.clientX);
-    node.dataset.startY = parseInt(evt.clientY);
-
-    document.onmousemove = function (evt) {
-      let startX = parseInt(node.dataset.startX);
-      let startY = parseInt(node.dataset.startY);
-      let offsetX = evt.clientX - startX;
-      let offsetY = evt.clientY - startY;
-
-      let top = parseInt(node.style.top);
-      let left = parseInt(node.style.left);
-
-      let xtimes = Math.floor(Math.abs(offsetX) / step) + (offsetX < 0 ? 0 : 1);
-      let leftOffset = offsetX % step;
-      if (Math.abs(leftOffset) >= (9)) {
-        leftOffset >= 0 ? (left += step * xtimes, startX += step * xtimes) :
-                          (left -= step * xtimes, startX -= step * xtimes);
-      }
-
-      let ytimes = Math.abs(Math.floor(offsetY / step)) + (offsetY < 0 ? 0 : 1);
-      let topOffset = offsetY % step;
-      if (Math.abs(topOffset) >= (9)) {
-        topOffset >= 0 ? (top += step * ytimes, startY += step * ytimes) :
-                         (top -= step * ytimes, startY -= step * ytimes);
-      }
-
-      console.log(top, left, startY, startX)
-      node.dataset.startX = startX;
-      node.dataset.startY = startY;
-      node.style.top = top + 'px';
-      node.style.left = left + 'px';
-    }
-    document.onmouseup = function (evt) {
-      document.onmousemove = null;
-      document.onmouseup = null;
+  let boot = makeCard({top: 0, left: 0,
+                       width: window.innerWidth, height: window.innerHeight}, main)
+  let grid = document.createElement('canvas');
+  grid.width = window.innerWidth;
+  grid.height = window.innerHeight;
+  boot.appendChild(grid);
+  let gctx = grid.getContext('2d');
+  gctx.globalAlpha = 0.2;
+  for (let x = 12; x < grid.width; x += 12) {
+    for (let y = 12; y < grid.height; y += 12) {
+      gctx.fillRect(x, y, 1, 1);
     }
   }
+  boot.style.border = '';
+  boot.style.cursor = 'crosshair';
+  let card = makeCard({top: 12, left: 12, width: 120, height: 120}, boot)
+  card.style.resize = 'both';
+  let card2 = makeCard({top: 12, left: 12, width: 120, height: 120}, boot)
+  putCodeMirror(emptycode, card)
+  putProseMirror(emptyprose, card2);
+
+
+
 }
--- src/utility.js
+++ src/utility.js
@@ -22,6 +22,8 @@ function makeCard(cardSpec, parent) {
   card.style.height = cardSpec.height + 'px';
 
   styleCard(card);
+  makeDraggable(card);
+  addResizer(card);
 
   parent.appendChild(card);
 
@@ -30,6 +32,120 @@ function makeCard(cardSpec, parent) {
 
 function styleCard(card) {
   card.style.border = '1px dashed black';
+  card.style.background = '#ffffff';
+  card.style.cursor = 'auto';
+}
+function addResizer(card, step=12) {
+  let resizer = document.createElement('div');
+  resizer.style.position = 'absolute'
+  resizer.style.top = parseInt(card.style.height) - 10 + 'px';
+  resizer.style.left = parseInt(card.style.width) - 10 + 'px';
+  resizer.style.width = '10px';
+  resizer.style.height = '10px';
+
+  resizer.style.background = '#eeeeee';
+  resizer.style.zIndex = 1;
+  resizer.style.cursor = 'nwse-resize';
+
+  resizer.onmousedown = function (evt) {
+    evt.stopPropagation();
+    resizer.dataset.startX = evt.clientX;
+    resizer.dataset.startY = evt.clientY;
+
+    document.onmousemove = function (evt) {
+      let width = parseInt(card.style.width);
+      let height = parseInt(card.style.height);
+
+      let startX = parseInt(resizer.dataset.startX);
+      let startY = parseInt(resizer.dataset.startY);
+
+      let dx = evt.clientX - startX;
+      let dy = evt.clientY - startY;
+
+      if (Math.abs(dx) >= step) {
+        let times = Math.floor(Math.abs(dx) / step) + (dx < 0 ? 0 : 1);
+        if (dx < 0) {
+          startX -= step * times;
+          width -= step * times;
+          resizer.style.left = parseInt(resizer.style.left) - step * times + 'px';
+        }
+        else { //positive
+          startX += step * times;
+          width += step * times;
+          resizer.style.left = parseInt(resizer.style.left) + step * times + 'px';
+        }
+      }
+      if (Math.abs(dy) >= step) {
+        let times = Math.floor(Math.abs(dy) / step) + (dy < 0 ? 0 : 1);
+        if (dy < 0) {
+          startY -= step * times;
+          height -= step * times;
+          resizer.style.top = parseInt(resizer.style.top) - step * times + 'px';
+        }
+        else { //positive
+          startY += step * times;
+          height += step * times;
+          resizer.style.top = parseInt(resizer.style.top) + step * times + 'px';
+        }
+      }
+
+      resizer.dataset.startX = startX;
+      resizer.dataset.startY = startY;
+      card.style.width = width + 'px';
+      card.style.height = height + 'px';
+    }
+    document.onmouseup = function (evt) {
+      document.onmousemove = null;
+      document.onmouseup = null;
+    }
+  }
+
+  card.appendChild(resizer);
+}
+function makeDraggable(card, step=12) {
+  //const step = 12; //constrain to 12pt grid
+  card.onmousedown = function (evt) {
+    evt.stopPropagation();
+    if(evt.altKey) {
+      card.style.cursor = 'move';
+      card.dataset.startX = evt.clientX;
+      card.dataset.startY = evt.clientY;
+
+      document.onmousemove = function (evt) {
+        let startX = parseInt(card.dataset.startX);
+        let startY = parseInt(card.dataset.startY);
+        let offsetX = evt.clientX - startX;
+        let offsetY = evt.clientY - startY;
+
+        let top = parseInt(card.style.top);
+        let left = parseInt(card.style.left);
+
+        let xtimes = Math.floor(Math.abs(offsetX) / step) + (offsetX < 0 ? 0 : 1);
+        let leftOffset = offsetX % step;
+        if (Math.abs(leftOffset) >= (9)) {
+          leftOffset >= 0 ? (left += step * xtimes, startX += step * xtimes) :
+            (left -= step * xtimes, startX -= step * xtimes);
+        }
+
+        let ytimes = Math.abs(Math.floor(offsetY / step)) + (offsetY < 0 ? 0 : 1);
+        let topOffset = offsetY % step;
+        if (Math.abs(topOffset) >= (9)) {
+          topOffset >= 0 ? (top += step * ytimes, startY += step * ytimes) :
+            (top -= step * ytimes, startY -= step * ytimes);
+        }
+
+        card.dataset.startX = startX;
+        card.dataset.startY = startY;
+        card.style.top = top + 'px';
+        card.style.left = left + 'px';
+      }
+      document.onmouseup = function (evt) {
+        document.onmousemove = null;
+        document.onmouseup = null;
+        card.style.cursor = 'auto';
+      }
+    }
+  }
 }
 
 function putCodeMirror(doc, parent) {
@@ -42,7 +158,7 @@ function putCodeMirror(doc, parent) {
   })
 
   // put it in parent's box
-  view.dom.style.height = parent.style.height;
+  view.dom.style.height = '100%';
   return view;
 }
 function putProseMirror(docObj, parent) {
@@ -59,7 +175,7 @@ function putProseMirror(docObj, parent) {
   view.dom.style.paddingRight = '0.5rem';
 
   // put it in parent's box
-  view.dom.style.height = parent.style.height;
+  view.dom.style.height = '100%';
   view.dom.style.overflow = 'auto';
   return view;
 }
@@ -84,6 +200,7 @@ function putConsole(target, parent) {}
 
 // different...
 function putLayout(layout, parent) {}
+function putContainer(parent) {}
 
 function execute(task) {
   let runner = stopify.stopifyLocally(task, {newMethod: 'direct'});
@@ -94,6 +211,7 @@ function execute(task) {
 
 export {makeCard, styleCard}
 export {putCodeMirror, putProseMirror}
-export {putButton, putCanvas, putConsole, putLayout}
+export {putButton, putCanvas, putConsole, putLayout, putContainer}
 export {execute}
 export {emptyprose, emptycode}
+export {makeDraggable}
